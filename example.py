0c9074dd4b34d101bb1c52b99b845f019e4e4482
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-08-03 19:21:01 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
+
+
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,17 +8,11 @@ def download_reel_by_id(reel_id):
     base_url = f'https://www.instagram.com/reel/{reel_id}/?&__a=1&__d=1'
 
     try:
-        # Send an HTTP request to get the JSON data
         response = requests.get(base_url)
         response.raise_for_status()
-
-        # Parse the JSON data to get the video URL
         json_data = response.json()
         video_url = json_data['graphql']['shortcode_media']['video_url']
-
-        # Download the video
         download_video(video_url)
-
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
     except KeyError:
@@ -23,19 +20,12 @@ def download_reel_by_id(reel_id):
 
 def download_video(video_url):
     try:
-        # Send an HTTP request to download the video
         response = requests.get(video_url)
         response.raise_for_status()
-
-        # Get the file name from the URL
         file_name = video_url.split('/')[-1]
-
-        # Save the video to the current directory
         with open(file_name, 'wb') as file:
             file.write(response.content)
-
         print(f"Video downloaded successfully as '{file_name}'.")
-
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
 
@@ -43,3 +33,5 @@ if __name__ == '__main__':
     # Replace 'ClwrpW1BB-R' with the Reel ID you want to download
     reel_id = 'ClwrpW1BB-R'
     download_reel_by_id(reel_id)
+
+
